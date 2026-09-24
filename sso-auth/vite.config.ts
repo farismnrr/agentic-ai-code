92e115ef888db3808637d1b8d00c63a094291503
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2026-09-24 22:34:21 +0700

    chore(sso): add daisyui styling foundation

--- sso-auth/vite.config.ts
+++ sso-auth/vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    tailwindcss(),
+    svelte()
+  ],
   build: {
     outDir: 'dist',
     emptyOutDir: true
